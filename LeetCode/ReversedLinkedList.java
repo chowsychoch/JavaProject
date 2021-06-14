c4a2a25b579ce31bf74371fdc3750d70ab7f4ade
chowsychoch <cho.cho@ucdconnect.ie>
2021-06-14 23:33:13 +0800

    Added Leetcode 206 Completed

--- LeetCode/ReversedLinkedList.java
+++ LeetCode/ReversedLinkedList.java
@@ -1,7 +1,24 @@
 package udemy.LeetCode;
 
 public class ReversedLinkedList {
-    public ListNode reverseList(ListNode head) {
+    public static void main(String[] args) {
+        ReversedLinkedList r = new ReversedLinkedList();
+        ListNode l1 = new ListNode(1, new ListNode(2,new ListNode(3, new ListNode(4, new ListNode(5)))));
 
+        r.reverseList(l1);
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode dummyhead = new ListNode(0, head);
+        ListNode current = head;
+        ListNode prev = null;
+        ListNode next = null;
+        while ( current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        dummyhead.next = prev;
+        return dummyhead.next;
     }
 }
