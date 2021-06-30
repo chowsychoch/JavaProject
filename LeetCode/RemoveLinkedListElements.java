5f0ac8cb8ecf9959f91e96c31979cfac7d2591dd
chowsychoch <cho.cho@ucdconnect.ie>
2021-06-30 11:20:48 +0800

    added leetcode 203

--- LeetCode/RemoveLinkedListElements.java
+++ LeetCode/RemoveLinkedListElements.java
@@ -2,6 +2,20 @@ package udemy.LeetCode;
 
 public class RemoveLinkedListElements {
     public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1, head);
+        if (head == null)return head;
+        ListNode current = head;
+        ListNode prev = dummy;
+        while (current !=null){
+            if(current.val == val){
+                ListNode next = current.next;
+                prev.next = next;
+            }else{
+                prev = current;
+            }
+            current = current.next;
+        }
 
+        return  dummy.next;
     }
 }
