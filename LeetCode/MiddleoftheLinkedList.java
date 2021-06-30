63b60349748fe485b326d4bcc181cdf93f8302b1
chowsychoch <cho.cho@ucdconnect.ie>
2021-06-30 11:37:26 +0800

    Added solution 876

--- LeetCode/MiddleoftheLinkedList.java
+++ LeetCode/MiddleoftheLinkedList.java
@@ -3,5 +3,13 @@ package udemy.LeetCode;
 public class MiddleoftheLinkedList {
     public ListNode middleNode(ListNode head) {
 
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while ( fast != null && fast.next !=null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
     }
 }
