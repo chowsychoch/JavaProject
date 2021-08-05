f98c0b6c4fa6a241a03830d5ed83b4a920ccecf2
chowsychoch <cho.cho@ucdconnect.ie>
2021-08-05 23:08:56 +0800

    added solution of leetcode 5

--- LeetCode/LongestPalindrome.java
+++ LeetCode/LongestPalindrome.java
@@ -1,4 +1,43 @@
 package udemy.LeetCode;
 
 public class LongestPalindrome {
+    public static void main(String[] args) {
+        LongestPalindrome n = new LongestPalindrome();
+        String ans = n.longestPalindrome("bba");
+        System.out.println(ans);
+    }
+    public String longestPalindrome(String s) {
+        if (s ==null || s.length() <1)return"";
+        int start = 0;
+        int end = 0;
+        for (int i =0; i < s.length() ;i++){
+            int len1 = expandFromMiddle(s,i,i);
+            int len2 = expandFromMiddle(s,i, i+1);
+            //System.out.println("i " + i);
+           //System.out.println("len 1: " + len1 +" len 2: " +len2);
+           int len = Math.max(len1, len2);
+            //int len = len1;
+
+            if ( len > end- start){
+                start = i - (len-1) /2 ;
+                end = i + len /2;
+                //System.out.println("start " + start);
+                //System.out.println("end" + end);
+            }
+        }
+        return s.substring(start,end+1);
+    }
+
+    private int expandFromMiddle(String s, int left, int right){
+        //System.out.println("s is " +s );
+        if ( left < 0 && right >= s.length()) return 0;
+
+        while (left>= 0 && right <s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        //System.out.println("left is " + left + " right is " + right);
+        //System.out.println("Result: " + (right - left - 1));
+        return right - left - 1;
+    }
 }
