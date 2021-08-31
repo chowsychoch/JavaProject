751edbec0d73c33e71c0b21f52a7755c5138ce19
chowsychoch <cho.cho@ucdconnect.ie>
2021-08-31 12:51:55 +0800

    added recursive approach

--- LeetCode/BinaryTreePostorderTraversal.java
+++ LeetCode/BinaryTreePostorderTraversal.java
@@ -1,10 +1,24 @@
 package udemy.LeetCode;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BinaryTreePostorderTraversal {
-
+    List<Integer> res = new LinkedList<>();
+    //recursion approach
     public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null )return res;
+        //visit left
+        if (root.left != null){
+            postorderTraversal(root.left);
+        }
+        //visit right
+        if (root.right !=null){
+            postorderTraversal(root.right);
+        }
+        //visit data
+        res.add(root.val);
 
+        return res;
     }
 }
