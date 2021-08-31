4610459b7d70970d44f980c00a8ccad65ab3231b
chowsychoch <cho.cho@ucdconnect.ie>
2021-08-31 14:01:32 +0800

    deleted main method

--- LeetCode/BinaryTreePreorderTraversal.java
+++ LeetCode/BinaryTreePreorderTraversal.java
@@ -7,9 +7,6 @@ import java.util.Stack;
 public class BinaryTreePreorderTraversal {
     List<Integer> res = new LinkedList<>();
 
-    public static void main(String[] args) {
-    }
-
     //Recursive Approach
     public List<Integer> preorderTraversal(TreeNode root) {
         if ( root == null) return res;
