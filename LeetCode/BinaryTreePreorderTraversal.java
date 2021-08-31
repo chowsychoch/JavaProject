ffd5663d25becab295b0291f5650522bee93457d
chowsychoch <cho.cho@ucdconnect.ie>
2021-08-31 12:32:22 +0800

    added recursive approach

--- LeetCode/BinaryTreePreorderTraversal.java
+++ LeetCode/BinaryTreePreorderTraversal.java
@@ -1,2 +1,26 @@
-package udemy.LeetCode;public class BinaryTreePreorderTraversal {
+package udemy.LeetCode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal {
+    List<Integer> res = new LinkedList<>();
+
+    public static void main(String[] args) {
+    }
+    //Recursive Approach
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if ( root == null) return res;
+        // visit data
+        res.add(root.val);
+        // visit left
+        if (root.left !=null){
+            preorderTraversal(root.left);
+        }
+        // visit right
+        if ( root.right !=null){
+            preorderTraversal(root.right);
+        }
+        return res;
+    }
 }
