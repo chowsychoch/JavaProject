634d8e24bfb071a4157a7e3f4e22e79a82d7faff
chowsychoch <cho.cho@ucdconnect.ie>
2021-09-09 16:59:14 +0800

    added max depth

--- LeetCode/SymmetricTree.java
+++ LeetCode/SymmetricTree.java
@@ -1,7 +1,16 @@
 package udemy.LeetCode;
 
+
 public class SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
+        if (root.left == null && root.right == null)return true;
+        if (root.left == null || root.right == null)return false;
+        return helperfunc(root.left, root.right);
+    }
 
+    private boolean helperfunc(TreeNode leftChild, TreeNode rightChild){
+        if ( leftChild == null && rightChild == null)return true;
+        if (leftChild == null || rightChild == null || leftChild.val != rightChild.val)return false;
+        return helperfunc(leftChild.left, rightChild.right) && helperfunc(leftChild.right, rightChild.left);
     }
 }
