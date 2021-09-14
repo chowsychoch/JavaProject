66322be9a4cc121575129f014430f5af88e8ee7e
chowsychoch <cho.cho@ucdconnect.ie>
2021-09-14 12:16:17 +0800

    added solution

--- LeetCode/BinaryTreePruning.java
+++ LeetCode/BinaryTreePruning.java
@@ -2,6 +2,14 @@ package udemy.LeetCode;
 
 public class BinaryTreePruning {
     public TreeNode pruneTree(TreeNode root) {
+        System.out.println("root. val " +root.val);
+        if ( root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
 
+        if (root.left == null && root.right == null && root.val == 0){
+            return null;
+        }
+        return root;
     }
 }
---
+++ LeetCode/LetterCombinationsofaPhoneNumber.java
@@ -0,0 +1,40 @@
+package udemy.LeetCode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LetterCombinationsofaPhoneNumber {
+    String[] keyboard = new String[]{"","","abc", "def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+        List<String> results = new LinkedList<>();
+        if (digits == null || digits.length() == 0)return results;
+        StringBuilder sb = new StringBuilder();
+        dfs(results, 0, sb, digits);
+        return results;
+    }
+
+    private void dfs(List<String> results, int index, StringBuilder sb, String digits){
+        //goals or base case
+        if (index == digits.length() ){
+            results.add(sb.toString());
+            return;
+        }
+        String letters = keyboard[digits.charAt(index) - '0'];
+        System.out.println(digits.charAt(index));
+        System.out.println("letter is " + letters);
+        for (int i =0; i < letters.length(); i++){
+            char c = letters.charAt(i);
+            System.out.println("appended: " + c);
+            sb.append(c);
+            System.out.println(sb);
+            dfs(results,index + 1 ,sb, digits);
+            System.out.println("before sb: " + sb);
+            System.out.println("To be deleted: "+sb.charAt(sb.length() -1 ));
+            System.out.println("deleted: "+sb.deleteCharAt(sb.length() -1 ));
+            System.out.println(" after sb: " + sb);
+
+            //sb.deleteCharAt(sb.length() -1 );
+        }
+    }
+}
