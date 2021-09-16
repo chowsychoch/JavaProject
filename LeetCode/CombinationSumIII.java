c3d175ddc5873a6939d5198b72f2a4f4486f9dd9
chowsychoch <cho.cho@ucdconnect.ie>
2021-09-16 12:41:52 +0800

    added combination III and subsetsII

--- LeetCode/CombinationSumIII.java
+++ LeetCode/CombinationSumIII.java
@@ -1,2 +1,39 @@
-package udemy.LeetCode;public class CombinationSumIII {
+package udemy.LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSumIII {
+    int[] key = {1,2,3,4,5,6,7,8,9};
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> results = new ArrayList<>();
+        if ( n == 0 )return results;
+        List<Integer> curr = new ArrayList<>();
+        backtracking(results, curr,k,n,0);
+        return results;
+    }
+
+    private void backtracking(List<List<Integer>>results, List<Integer> curr, int k, int n,int index){
+        //
+        if ( n < 0 ){
+            return;
+        }
+
+        if ( n == 0 && curr.size()  == k  ){
+            // System.out.println("h");
+            results.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for ( int i = index; i < key.length ; i++ ){
+            System.out.println(key[i]);
+            curr.add(key[i]);
+            System.out.println(curr.toString());
+            backtracking(results,curr, k, n - key[i], i+ 1);
+            curr.remove(curr.size() - 1 );
+
+        }
+
+    }
 }
--- LeetCode/SubsetsII.java
+++ LeetCode/SubsetsII.java
@@ -1,2 +1,28 @@
-package udemy.LeetCode;public class SubsetsII {
+package udemy.LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        Arrays.sort(nums);
+        if ( nums == null || nums.length == 0) return results;
+        backtracking(results, 0, curr, nums);
+        return results;
+    }
+
+    private void backtracking(List<List<Integer>> results, int index, List<Integer> curr, int[] nums){
+        results.add(new ArrayList<>(curr));
+
+        for ( int i = index; i < nums.length ; i++){
+            if ( i == index || nums[i] != nums[ i - 1]){
+                curr.add(nums[i]);
+                backtracking(results, i+ 1, curr, nums);
+                curr.remove(curr.size() - 1 );
+            }
+        }
+    }
 }
