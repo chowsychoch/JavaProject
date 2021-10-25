14084139325b7150bd9657bfa886a6f9b05fe528
chowsychoch <cho.cho@ucdconnect.ie>
2021-10-25 13:45:58 +0800

    added solution 2

--- LeetCode/LinkedListCycleII.java
+++ LeetCode/LinkedListCycleII.java
@@ -19,4 +19,47 @@ public class LinkedListCycleII {
 
 
     }
+
+    private ListNode getIntersect(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if ( slow == fast){
+                return slow;
+            }
+        }
+
+
+        return null;
+
+    }
+
+
+    public ListNode detectCycle(ListNode head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode Intersect = getIntersect(head);
+
+        if (Intersect == null) {
+            return null;
+        }
+
+        ListNode ptr1 = head;
+        ListNode ptr2 = Intersect;
+
+        while (ptr1 != ptr2) {
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+
+        return ptr1;
+
+
+    }
 }
